856fb07433e6587344e108196249d25b72912be0
priscillaahn <ahnpriscilla@gmail.com>
2026-03-31 20:00:38 -0400

    load as csv logic

---
+++ flashcard_app/utils/load.py
@@ -0,0 +1,51 @@
+from typing import Optional
+
+import pandas as pd
+
+from flashcard_app.models.flashcards import CardSide, Card, Deck
+
+
+def load_csv(csv_filepath: str) -> pd.DataFrame:
+    """Loads csv as pandas dataframe."""
+    try:
+        data = pd.read_csv(csv_filepath)
+        return data
+    except Exception as e:
+        raise RuntimeError(f"Failed to load CSV '{csv_filepath}'") from e
+
+def convert_tags_to_list(tags_str: str) -> list[str]:
+    """Convert comma-separated tags string from CSV to a list."""
+    return [t.strip() for t in tags_str.split(",") if t.strip()]
+
+def load_cards_from_csv(csv_filepath: str, lang_front: str, lang_back: str) -> list[Card]:
+    """Creates a list of card objects from a csv."""
+    data = load_csv(csv_filepath=csv_filepath)
+
+    cards: list[Card] = []
+    for row in data.itertuples(index=False):
+        card = Card(
+            front=CardSide(
+                text=str(row.front),
+                language=lang_front,
+            ),
+            back=CardSide(
+                text=str(row.back),
+                language=lang_back,
+                ),
+            part_of_speech=str(row.part_of_speech),
+            tags=convert_tags_to_list(str(row.tags)) if pd.notna(row.tags) else []
+        )
+        cards.append(card)
+    
+    return cards
+
+def create_deck(
+    cards: list[Card],
+    name: str,
+    description: Optional[str] = None,
+) -> Deck:
+    """Creates deck from a list of cards."""
+    return Deck(
+        name=name, description=description, cards=cards
+    )
+
